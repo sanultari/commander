70794c993ede539243b77f20a4716afd551dfb3b
moonifly <moonifly@gmail.com>
2012-11-14 02:45:22 +0900

    옵션 체크용 샘플 파일 추가

---
+++ spec/option_sample.rb
@@ -0,0 +1,67 @@
+# coding: utf-8
+
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+
+class Hello
+  attr_accessor :name
+
+  def hello
+    puts "Hello, #{self.name}"
+  end
+end
+
+class Goodbye
+  def bye name, options = nil
+    if options != nil && options.length > 0
+      options.each do |option|
+        puts "Goodbye #{name} options = #{option}"
+      end
+    else
+      puts "Goodbye #{name}"
+    end
+  end
+
+
+  def bye2
+    puts "잘가라"
+  end
+
+end
+
+
+
+
+class AnotherRunner
+  include SanUltari::CommandDescriptor
+
+  @options = [:a, :all, :l, :u]
+
+  #, desc: 'goodbye command'
+  map :bye, Goodbye, @options
+  param :bye, :name, type: :parameter
+
+  map :bye2, Goodbye
+end
+
+class Runner
+  include SanUltari::CommandDescriptor
+
+  default :hello
+
+  map :hello, Hello, desc: 'hello command'
+  desc :hello, 'hello'
+  # type: :attribute is default
+  param :hello, :name, require: true, default: 'World', type: :accessor
+  # second parameter is optional. second parameter can remove for importing all commands
+  import AnotherRunner
+  # another importing method. imported commands has group. commands having same name can be distinguish
+  # last optional parameter is same functionally with above method
+  group :say, AnotherRunner
+end
+
+Runner.run ARGV
+
+
+# ruby sample.rb -v -h --help hello --test tim
+#  > Hello, tim
