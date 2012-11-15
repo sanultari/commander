d45af65ace5fae92152e51396fb898b9fcebe687
moonifly <moonifly@gmail.com>
2012-11-15 17:54:26 +0900

    잘못된 require 제거

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'sanultari-commander'
-require 'sanultari/command_option'
-require 'sanultari/command_option_manager'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
