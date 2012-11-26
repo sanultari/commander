622ac593adf0c2facc4bb3c9e35fe8470d9c5144
moonifly <moonifly@gmail.com>
2012-11-26 15:27:34 +0900

    option_dic, option_dic_spec 추가. command_parameter_spec 에 불필요 require 제거

---
+++ lib/sanultari/option_dic.rb
@@ -0,0 +1,47 @@
+require 'sanultari/option'
+
+class SanUltari::OptionDic
+  attr_reader :options, :abbrs
+
+
+  def initialize
+    @options = {}
+    @abbrs = {}
+  end
+
+  def add name, options = nil
+    if @options.include? name
+      puts "Option name duplicate. option name = #{name}."
+    else
+      option = SanUltari::Option.new name, options
+      @options[option.name.to_sym] = option
+      add_abbr option
+    end
+  end
+
+
+  def name_include? name
+    @options.include? name
+  end
+
+  def abbr_include? abbr
+    @abbrs.include? abbr
+  end
+
+
+
+  private
+
+  def add_abbr option
+    if option.abbr == nil
+      return
+    end
+
+    if abbr_include? option.abbr
+      puts "Option abbr duplicate. abbr value = #{option.abbr}."
+    else
+      @abbrs[option.abbr.to_sym] = option
+    end
+  end
+
+end
---
+++ spec/option_dic_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe "OptionDic Work" do
+
+  context "Add option work." do
+
+    it "add option. this options is not duplicate. normal option." do
+      option_dic = SanUltari::OptionDic.new
+      option_dic.add :option_name
+      option_dic.options.length.should eq(1)
+    end
+
+    it "add option. this options is not duplicate. more option value.. abbr, require" do
+      option_dic = SanUltari::OptionDic.new
+      option_dic.add :option_name, abbr: :n, require: false
+      option_dic.options.length.should eq(1)
+    end
+
+    it "add option fail. option name is duplicate" do
+      option_dic = SanUltari::OptionDic.new
+      option_dic.add :option_name
+      option_dic.add :option_name
+      option_dic.options.length.should eq(1)
+    end
+
+  end
+
+end
